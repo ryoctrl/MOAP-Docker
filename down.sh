64f804d21d756848e9f38f8443a28ef25d7d42f7
ryoctrl@gmail.com <ryoctrl@gmail.com>
2020-07-11 16:27:32 +0900

    catapult追加

--- down.sh
+++ down.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+echo "MOAPをシャットダウンしています"
+
 docker-compose down
+
+echo "catapultをシャットダウンしています"
+
+cd catapult/cmds/bootstrap && docker-compose down
+
+echo "MOAPとcatapultをシャットダウンしました"
+
