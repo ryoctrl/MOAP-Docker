f280c4565ebdf7bdf5572ef3dc3e0095d085d70b
ryoctrl <ryoctrl@gmail.com>
2020-07-20 23:12:24 +0900

    fix path

--- cmds/init.sh
+++ cmds/init.sh
@@ -17,15 +17,15 @@ git clone https://github.com/tech-bureau/catapult-service-bootstrap.git catapult
 #cd MOAP-Management && npm i && cd ..
 
 echo "MOAPシステムを起動しています"
-./up.sh
+$SCRIPT_DIR/up.sh
 
 # 初回起動時に非同期にDBの初期化が行われるため、しばらく待ってからmigrationを実行する
 echo "MOAPシステムを起動しました."
 echo "データベースの初期化を待機しています."
 sleep 30
-./migrate.sh
+$SCRIPT_DIR/migrate.sh
 
 echo "データベースを初期化しました."
 
-./logs.sh
+$SCRIPT_DIR/logs.sh
 
