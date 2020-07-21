5b2479c949f9198090e1747efd2f854f4d74875a
ryoctrl <ryoctrl@gmail.com>
2020-07-21 16:13:13 +0900

    update

--- cmds/migrate.sh
+++ cmds/migrate.sh
@@ -3,3 +3,4 @@
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE:-$0}"); pwd)
 
 cd $SCRIPT_DIR/../ && docker-compose exec backend ./node_modules/.bin/sequelize db:migrate
+cd $SCRIPT_DIR/../ && docker-compose exec queue ./node_modules/.bin/sequelize db:migrate
