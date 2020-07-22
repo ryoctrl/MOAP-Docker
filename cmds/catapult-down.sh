b51681cba7b3e4135b81f9d1bf7dab316659bf78
ryoctrl <ryoctrl@gmail.com>
2020-07-22 17:10:58 +0900

    fix

--- cmds/catapult-down.sh
+++ cmds/catapult-down.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE:-$0}"); pwd)
 
 echo "catapultをシャットダウンしています"
 
-cd $SCRIPT_DIR/../catapult/cmds && ./stop-all
+cd $SCRIPT_DIR/../catapult/ && ./cmds/stop-all
 
