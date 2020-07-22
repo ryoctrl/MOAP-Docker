1535095a9440fc635aafa5849417b22ba977a792
ryoctrl <ryoctrl@gmail.com>
2020-07-22 16:35:48 +0900

    wip update

--- cmds/init.sh
+++ cmds/init.sh
@@ -10,7 +10,8 @@ git clone https://git.mosin.jp/git/mosin/MOAP-Backend.git
 git clone https://git.mosin.jp/git/mosin/MOAP-Front.git
 git clone https://git.mosin.jp/git/mosin/MOAP-Management.git
 git clone https://git.mosin.jp/git/mosin/Queue-Backend.git MOAP-Queue
-git clone https://github.com/tech-bureau/catapult-service-bootstrap.git catapult -b 0.8.0.3
+#git clone https://github.com/tech-bureau/catapult-service-bootstrap.git catapult -b 0.8.0.3
+git clone https://github.com/tech-bureau/catapult-service-bootstrap.git catapult -b 0.9.0.1
 
 echo "NEM Catapultを初期化しています"
 $SCRIPT_DIR/catapult-up.sh
