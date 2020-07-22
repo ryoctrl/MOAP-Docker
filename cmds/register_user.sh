1bdbfe6e1eac7b74adf4e185334058e3e4dc48e4
ryoctrl <ryoctrl@gmail.com>
2020-07-23 02:54:43 +0900

    bugfix

--- cmds/register_user.sh
+++ cmds/register_user.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 body="{\"id\": \"$1\"}"
-curl -X POST -H "Content-Type: application/json" -d "$body" localhost:9250/api/user/register
+curl -X POST -H "Content-Type: application/json" -d "$body" http://localhost:9250/api/user/register
